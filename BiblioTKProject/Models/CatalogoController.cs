d8e31ea8878945b5463019f81937e55119cd3eb7
esurmay <esurmay@gmail.com>
2016-07-04 10:49:55 +0200

    Modificaciom (MenuController, _Layaout.cshtml, Catalogo.cshtml, Catalogo/ClasificacionPrincipalMenu.cshtml, index.cshtml, top10.cshtml). Nuevo catalogoGetData.js.

--- BiblioTKProject/Models/CatalogoController.cs
+++ BiblioTKProject/Models/CatalogoController.cs
@@ -4,21 +4,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Caching;
 
 namespace BiblioTKProject.Models
 {
     public class CatalogoController : Controller
     {
         // GET: Catalogo
+        [OutputCache(Duration = 30)]
         public ActionResult Index()
         {
             CatalogoIndexModelView modelo = new CatalogoIndexModelView();
             CatalogoNegocio objCatalogo = new CatalogoNegocio();
-            modelo.Libros  = objCatalogo.listarCatalogoPorSPPaginado(10, 7);
-            modelo.ClasificacionPrincipalMenu = objCatalogo.ListarClasificaionesPrincipales();
-            modelo.Top10Menu = objCatalogo.ListarTop10(DateTime.Now.AddDays(-30));
+            MenuNegocio objMenu = new MenuNegocio();
+            //modelo.Libros = objCatalogo.listarCatalogoPorSPPaginado(10, 1);
+            modelo.ClasificacionPrincipalMenu = objMenu.ListarClasificaionesPrincipales();
+            modelo.Top10Menu = objMenu.ListarTop10(FuncionesVB.FuncionesGenerales.Takoma_UTCToMexCentral());
+            modelo.NuevosMaterialesMenu = objMenu.NuevosMateriales();
 
             return View(modelo);
         }
+
+         public ActionResult GetData(int pageIndex, int pageSize)
+        {
+             CatalogoNegocio objCatalogo = new CatalogoNegocio();
+             var Libros = objCatalogo.listarCatalogoPorSPPaginado(pageSize, pageIndex);
+
+            return Json(Libros.ToList(), JsonRequestBehavior.AllowGet);
+        }
+
+
+
     }
+
+
+
 }
---
+++ BiblioTKProject/Scripts/catalogoGetData.js
@@ -0,0 +1,33 @@
+﻿var pageSize = 10;
+var pageIndex = 0;
+
+function GetData() {
+    $.ajax({
+        type: 'GET',
+        url: '/Catalogo/GetData',
+        data: { "pageindex": pageIndex, "pagesize": pageSize },
+        dataType: 'json',
+        success: function (data) {
+            if (data != null) {
+                for (var i = 0; i < data.length; i++) {
+
+                    $("#DivLibros").append("<a href='#' class='list-group-item'>" +
+                        "<h4 class='li-head'>" + data[i].cat_Titulo + "</h4>" +
+                        "<p class='li-sub'>" + data[i].autor_nombrecompleto + "</p>" +
+                        "<p class='li-sub'>" + data[i].cat_Año + "</p>" +
+                    "</a>");
+                }
+                pageIndex++;
+            }
+        },
+        beforeSend: function () {
+            $("#progress").show();
+        },
+        complete: function () {
+            $("#progress").hide();
+        },
+        error: function (request, status, error) {
+            alert("ha ocurrido algo. Intenta de nuevo. " + request.responseText + error);
+        }
+    });
+}

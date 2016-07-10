eca9c5e6e129ecc95aa943e9eb1b0d5d756f6a2c
esurmay <esurmay@gmail.com>
2016-07-10 17:24:20 +0200

    limpieza de codigo y referencias agregadas

--- BiblioTK.MVC/Scripts/catalogoGetData.js
+++ BiblioTK.MVC/Scripts/catalogoGetData.js
@@ -1,32 +1,15 @@
 ﻿var pageSize = 10;
 var pageIndex = 0;
-var image = "";
+ 
 function GetData() {
     $.ajax({
-        type: 'GET',
+        type: 'POST',
         url: '/Catalogo/GetData',
         data: { "pageindex": pageIndex, "pagesize": pageSize },
-        dataType: 'json',
-        success: function (data) {
-            if (data != null) {
-                for (var i = 0; i < data.length; i++) {
-                    
-                    var shtml = "<ul class='media-list main-list list-group-item'>" +
-			          "<li class='media'>" +
-			            "<a class='pull-left' href='#'>" +
-			             " <img class='media-object img-responsive' src='" + data[i].imagenRuta + "' alt='...'>" +
-			            "</a>" +
-			            "<div class='media-body'>" +
-			                "<a class='li-head' href='#'><h4>" + data[i].cat_Titulo + "</h4></a>" +
-                            "<p class='li-sub'>" + data[i].autor_nombrecompleto + "</p>" +
-                            "<p class='li-sub'>" + data[i].cat_Año + "</p>" +
-			          "  </div>" +
-			         " </li>		" +	 
-			        "</ul>";
-                 $("#DivLibros").append(shtml);
-                }
-                pageIndex++;
-            }
+        dataType: 'html',
+        success: function (result) {
+            $("#DivLibros").append(result);
+            pageIndex++;
         },
         beforeSend: function () {
             $("#progress").show();

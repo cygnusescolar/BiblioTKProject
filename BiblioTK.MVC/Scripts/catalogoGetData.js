c8381176de3d0bc5b67fff3cfbaddaa3d41840cd
esurmay <esurmay@gmail.com>
2016-09-27 17:00:05 +0200

    CargarCatalogoFiltrado: 1.Extension de parametros; 2. Modificacion de bloque de codigo

--- BiblioTK.MVC/Scripts/catalogoGetData.js
+++ BiblioTK.MVC/Scripts/catalogoGetData.js
@@ -34,25 +34,27 @@ function CargarCatalogo() {
 }
 
 
-function CargarCatalogoFiltrado(elementoA) {
-    if (filtrado == false) {
-        pageIndex = 0;
+function CargarCatalogoFiltrado(elementoA, filtroMenu) {
+    if (filtrado == false || filtroMenu) {
+        pageIndex = 0; $("#searchText").val("");
     }
 
     var niveles = (elementoA != undefined) ? $(elementoA).attr("level") : nivel;
     if(nivel != niveles)
     { $("#DivLibros").html(''); pageIndex = 0; nivel = niveles; }
     
-    $.ajax({
+    var searchText = $("#searchText").val();
+    if (searchText == "") {
+        $.ajax({
         type: 'POST',
         url: '/Catalogo/CargarCatalogoFiltrado',
         data: { "level": niveles, "pagesize": pageSize, "pageindex": pageIndex },
         dataType: 'html',
         success: function (result) {
-            var htotalLibros = $($.parseHTML(result)).filter("#htotalLibros");
+            /*var htotalLibros = $($.parseHTML(result)).filter("#htotalLibros");
             var total = htotalLibros.val();
+            $('#h4Librosencontrados').text(total);*/
 
-            $('#h4Librosencontrados').text(total);
             $("#DivLibros").append(result);
             pageIndex++;
             filtrado = true;
@@ -69,5 +71,43 @@ function CargarCatalogoFiltrado(elementoA) {
             alert("ha ocurrido algo. Intenta de nuevo. " + request.responseText + error);
         }
     });
+    }
+    else { 
+        $.ajax({
+            type: 'POST',
+            url: '/Catalogo/BuscarPorTitulo',
+            data: { "searchText": searchText, "pagesize": pageSize, "pageindex": pageIndex },
+            dataType: 'html',
+            success: function (result) {
+                /*var htotalLibros = $($.parseHTML(result)).filter("#htotalLibros");
+                var total = htotalLibros.val();
+                $('#h4Librosencontrados').text(total);*/
 
+                $("#DivLibros").append(result);
+                pageIndex++;
+                filtrado = true;
+            },
+            beforeSend: function () {
+                if (pageIndex == 0)
+                { $("#DivLibros").html(''); }
+                $("#progress").show();
+            },
+            complete: function () {
+                $("#progress").hide();
+            },
+            error: function (request, status, error) {
+                alert("ha ocurrido algo. Intenta de nuevo. " + request.responseText + error);
+            }
+        });
+
+    }
 }
+
+function onSubmitFeedbackBegin(context) {
+     pageIndex = 1; $(window).scrollTop(0);
+ }
+
+function onSuccess(context) {
+     filtrado = true;
+}
+ 

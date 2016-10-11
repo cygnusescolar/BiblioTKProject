1c29ac8995888fce2a28cc07757427c49df0fd7a
esurmay <esurmay@gmail.com>
2016-10-11 15:51:46 +0200

    cambios

--- BiblioTK.MVC/Scripts/catalogoGetData.js
+++ BiblioTK.MVC/Scripts/catalogoGetData.js
@@ -34,40 +34,83 @@ function CargarCatalogo() {
 }
 
 
-function CargarCatalogoFiltrado(elementoA) {
-    if (filtrado == false) {
-        pageIndex = 0;
+function CargarCatalogoFiltrado(elementoA, filtroMenu) {
+    if (filtrado == false || filtroMenu) {
+        pageIndex = 0; $("#searchText").val("");
     }
 
     var niveles = (elementoA != undefined) ? $(elementoA).attr("level") : nivel;
-    if(nivel != niveles)
+    if (nivel != niveles)
     { $("#DivLibros").html(''); pageIndex = 0; nivel = niveles; }
-    
-    $.ajax({
-        type: 'POST',
-        url: '/Catalogo/CargarCatalogoFiltrado',
-        data: { "level": niveles, "pagesize": pageSize, "pageindex": pageIndex },
-        dataType: 'html',
-        success: function (result) {
-            var htotalLibros = $($.parseHTML(result)).filter("#htotalLibros");
-            var total = htotalLibros.val();
 
-            $('#h4Librosencontrados').text(total);
-            $("#DivLibros").append(result);
-            pageIndex++;
-            filtrado = true;
-        },
-        beforeSend: function () {
-            if(pageIndex == 0)
-            {   $("#DivLibros").html('');}
-            $("#progress").show();
-        },
-        complete: function () {
-            $("#progress").hide();
-        },
-        error: function (request, status, error) {
-            alert("ha ocurrido algo. Intenta de nuevo. " + request.responseText + error);
-        }
-    });
+    var searchText = $("#searchText").val();
+    if (searchText == "" || searchText == undefined) {
+        $.ajax({
+            type: 'POST',
+            url: '/Catalogo/CargarCatalogoFiltrado',
+            data: { "level": niveles, "pagesize": pageSize, "pageindex": pageIndex },
+            dataType: 'html',
+            success: function (result) {
+                /*var htotalLibros = $($.parseHTML(result)).filter("#htotalLibros");
+                var total = htotalLibros.val();
+                $('#h4Librosencontrados').text(total);*/
 
+                $("#DivLibros").append(result);
+                pageIndex++;
+                filtrado = true;
+            },
+            beforeSend: function () {
+                if (pageIndex == 0)
+                { $("#DivLibros").html(''); }
+                $("#progress").show();
+            },
+            complete: function () {
+                $("#progress").hide();
+            },
+            error: function (request, status, error) {
+                alert("ha ocurrido algo. Intenta de nuevo. " + request.responseText + error);
+            }
+        });
+    }
+    else {
+        $.ajax({
+            type: 'POST',
+            url: '/Catalogo/BuscarPorTitulo',
+            data: { "searchText": searchText, "pagesize": pageSize, "pageindex": pageIndex },
+            dataType: 'html',
+            success: function (result) {
+                /*var htotalLibros = $($.parseHTML(result)).filter("#htotalLibros");
+                var total = htotalLibros.val();
+                $('#h4Librosencontrados').text(total);*/
+
+                $("#DivLibros").append(result);
+                pageIndex++;
+                filtrado = true;
+            },
+            beforeSend: function () {
+                if (pageIndex == 0)
+                { $("#DivLibros").html(''); }
+                $("#progress").show();
+            },
+            complete: function () {
+                $("#progress").hide();
+            },
+            error: function (request, status, error) {
+                alert("ha ocurrido algo. Intenta de nuevo. " + request.responseText + error);
+            }
+        });
+
+    }
+}
+
+function onSubmitFeedbackBegin(context) {
+    pageIndex = 1; $(window).scrollTop(0);
+}
+
+function onSuccess(context) {
+    var searchText = $("#searchText").val();
+    if (searchText == "")
+        filtrado = false;
+    else
+        filtrado = true;
 }

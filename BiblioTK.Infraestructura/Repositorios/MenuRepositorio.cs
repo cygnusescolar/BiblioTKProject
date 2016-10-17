aac9cce4d4be5225496a6c2c554cbc36490d3ef7
esurmay <esurmay@gmail.com>
2016-10-17 15:56:11 +0200

    MenuRepositorio: - Se ha modificado el metodo para retornar null en caso de que se genere una excepcion

--- BiblioTK.Infraestructura/Repositorios/MenuRepositorio.cs
+++ BiblioTK.Infraestructura/Repositorios/MenuRepositorio.cs
@@ -13,21 +13,35 @@ namespace BiblioTK.Infraestructura.Repositorios
     {
         public List<CatalogoResult> ListarTop24(DateTime fecha)
         {
-            using (CygnusBiblioTKv2Entities context = new CygnusBiblioTKv2Entities())
+            try
             {
+                using (CygnusBiblioTKv2Entities context = new CygnusBiblioTKv2Entities())
+                {
 
-                var fechaparam = new SqlParameter("@D1", System.Data.SqlDbType.DateTime);
-                fechaparam.Value = fecha;
-                return context.Database.SqlQuery<CatalogoResult>("Top24 @D1", fechaparam).ToList();
+                    var fechaparam = new SqlParameter("@D1", System.Data.SqlDbType.DateTime);
+                    fechaparam.Value = fecha;
+                    return context.Database.SqlQuery<CatalogoResult>("Top24 @D1", fechaparam).ToList();
+                }
+            }
+            catch (Exception)
+            {
+                return null;
             }
         }
 
         public List<ClasficacionPrincipalResult> ListarClasificaionesPrincipales()
         {
-            using (CygnusBiblioTKv2Entities context = new CygnusBiblioTKv2Entities())
+            try
             {
+                using (CygnusBiblioTKv2Entities context = new CygnusBiblioTKv2Entities())
+                {
 
-                return context.Database.SqlQuery<ClasficacionPrincipalResult>("SP_CantPorClasificacion").ToList();
+                    return context.Database.SqlQuery<ClasficacionPrincipalResult>("SP_CantPorClasificacion").ToList();
+                }
+            }
+            catch (Exception)
+            {
+                return null;
             }
         }
 
